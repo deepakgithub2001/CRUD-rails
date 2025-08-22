9c8a3dfa864f4c792bb6b791665d261cc4391e64
deepakgithub2001 <deepakchaudhary.cse2001@gmail.com>
2025-08-22 11:08:01 +0530

    fixed syntax error

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,5 @@ class Book < ApplicationRecord
     has_one_attached :book_image
     belongs_to :author
     validates :title, presence: true, uniqueness: true
-    validates :body, presence: true,
+    validates :body, presence: true
 end 
